47f255727d5e9f097a471540afacc73d26fb0712
Moises Alvarado <moisesalvarado84@gmail.com>
2023-10-24 17:07:07 -0600

    Using the useRef hook

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -6,12 +6,11 @@ import './index.css';
 //import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
 // import { SimpleForm } from './02-useEffect/SimpleForm';
 // import { SimpleFormWithCustomHook } from './02-useEffect/SimpleFormWithCustomHook';
-import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
+// import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
+import { FocusScreen } from './04-useRef/FocusScreen';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-
-    <MultipleCustomHooks />
-
+    <FocusScreen />
   </React.StrictMode>
 );
