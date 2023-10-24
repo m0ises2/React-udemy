230fd35d288c5704fa1c098f99c4bb4d3183ffb2
Moises Alvarado <moisesalvarado84@gmail.com>
2023-10-24 17:27:11 -0600

    Using the useLayoutEffect hook

--- 05-hook-app/src/03-examples/Quote.jsx
+++ 05-hook-app/src/03-examples/Quote.jsx
@@ -1,11 +1,33 @@
 import PropTypes from "prop-types";
+import { useLayoutEffect, useRef, useState } from "react";
 
 export const Quote = ({ author, quote }) => {
+  const pRef = useRef();
+
+  const [boxSize, setBoxSize] = useState({
+    height: 0,
+    width: 0
+  });
+
+  useLayoutEffect(() => {
+    const { height, width } = pRef.current.getBoundingClientRect();
+    setBoxSize({
+      height,
+      width
+    });
+  }, [quote]);
+
   return (
-    <blockquote className="blockquote text-end">
-        <p className="mb-1"> { quote } </p>
+    <blockquote
+      className="blockquote text-end"
+      style={{ display: 'flex' }}
+    >
+        <p ref={ pRef } className="mb-1"> { quote } </p>
         <footer className="blockquote-footer"> { author } </footer>
-    </blockquote>
+
+        <code> {JSON.stringify(boxSize)} </code>
+
+    </blockquote>    
   )
 }
 
--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import { HooksApp } from "./HooksApp";
 import './index.css';
+import { Layout } from './05-useLayoutEffect/Layout';
 // import { CounterApp } from './01-useState/CounterApp';
 //import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
 // import { SimpleForm } from './02-useEffect/SimpleForm';
 // import { SimpleFormWithCustomHook } from './02-useEffect/SimpleFormWithCustomHook';
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
-import { FocusScreen } from './04-useRef/FocusScreen';
+// import { FocusScreen } from './04-useRef/FocusScreen';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <FocusScreen />
+    <Layout />
   </React.StrictMode>
 );
