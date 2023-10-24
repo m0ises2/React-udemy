ca0c10a2ad50a85f0239041ea27df621682a824d
Moises Alvarado <moisesalvarado84@gmail.com>
2023-10-24 17:49:05 -0600

    Using the memo function

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import { HooksApp } from "./HooksApp";
 import './index.css';
-import { Layout } from './05-useLayoutEffect/Layout';
+// import { Layout } from './05-useLayoutEffect/Layout';
+import { Memorize } from './06-memos/Memorize';
 // import { CounterApp } from './01-useState/CounterApp';
 //import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
 // import { SimpleForm } from './02-useEffect/SimpleForm';
@@ -12,6 +13,6 @@ import { Layout } from './05-useLayoutEffect/Layout';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Layout />
+    <Memorize />
   </React.StrictMode>
 );
