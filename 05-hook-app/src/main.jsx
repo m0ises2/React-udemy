bfffe2fa730c5950a033e222327e24677f0b25f6
Moises Alvarado <moisesalvarado84@gmail.com>
2023-10-31 17:37:35 -0600

    Using react memo and useCallback para memorizar los componentes y estados

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -5,7 +5,8 @@ import './index.css';
 // import { Layout } from './05-useLayoutEffect/Layout';
 // import { Memorize } from './06-memos/Memorize';
 // import { MemoHook } from './06-memos/memoHook';
-import { CallbackHook } from './06-memos/CallbackHook';
+// import { CallbackHook } from './06-memos/CallbackHook';
+import { Padre } from './07-tarea-memo/Padre';
 // import { CounterApp } from './01-useState/CounterApp';
 //import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
 // import { SimpleForm } from './02-useEffect/SimpleForm';
@@ -15,6 +16,6 @@ import { CallbackHook } from './06-memos/CallbackHook';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <CallbackHook />
+    <Padre />
   // </React.StrictMode>
 );
