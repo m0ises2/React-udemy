b6a020b3b5d6ab342e2f691fa3315e6fe8b198e2
Moises Alvarado <moisesalvarado84@gmail.com>
2024-01-15 17:29:14 -0600

    Adding unit tests to the main page component

---
+++ 05-hook-app/test/09-useContext/MainApp.test.jsx
@@ -0,0 +1,36 @@
+import { fireEvent, render, screen } from '@testing-library/react'
+import { MainApp } from '../../src/09-useContext/MainApp';
+import { UserProvider } from '../../src/09-useContext/context/UserProvider';
+import { MemoryRouter } from 'react-router-dom';
+
+describe('Pruebas en MainApp', () => {
+  test('debe mostrar el home page', () => {
+    render(
+      <MemoryRouter>
+        <MainApp/>
+      </MemoryRouter>
+    );
+
+    expect(screen.getByText('Home Page')).toBeTruthy();
+  });
+
+  test('debe mostrar el login page', () => {
+    render(
+      <MemoryRouter initialEntries={['/login']}>
+        <MainApp/>
+      </MemoryRouter>
+    );
+
+    expect(screen.getByText('Login Page')).toBeTruthy();
+  });
+
+  test('debe mostrar el about page', () => {
+    render(
+      <MemoryRouter initialEntries={['/about']}>
+        <MainApp/>
+      </MemoryRouter>
+    );
+
+    expect(screen.getByText('About Page')).toBeTruthy();
+  });
+});
