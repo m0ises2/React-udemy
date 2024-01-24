a75378faa3949746cf207de96084fe83a2bd081c
Moises Alvarado <moisesalvarado84@gmail.com>
2024-01-24 17:54:09 -0600

    adding the heroes list

---
+++ 07-heroes-spa/src/heroes/components/HeroList.jsx
@@ -0,0 +1,17 @@
+import { getHeroesByPublisher } from "../helpers/getHeroesByPublisher"
+
+export const HeroList = ({ publisher }) => {
+    const heroes = getHeroesByPublisher(publisher);
+    
+    return (
+        <ul>
+            { heroes && heroes.map(hero => {
+                return <>
+                    <li key={ hero.id }>
+                        { hero.superhero }
+                    </li>
+                </>
+            }) }
+        </ul>
+    )
+}
