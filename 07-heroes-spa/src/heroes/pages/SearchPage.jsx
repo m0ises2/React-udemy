fff9da9f2111ffda58a3b3ce4e53c4e0c6817186
Moises Alvarado <moisesalvarado84@gmail.com>
2024-01-30 17:24:37 -0600

    Adding the useForm custom hook to the SearchPage. Extracted queryParams on form submission

--- 07-heroes-spa/src/heroes/pages/SearchPage.jsx
+++ 07-heroes-spa/src/heroes/pages/SearchPage.jsx
@@ -1,6 +1,60 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import queryString from "query-string";
+import { HeroCard } from "../components";
+import { useForm } from "../../hooks/useForm";
 
 export const SearchPage = () => {
+  const navigate = useNavigate();
+
+  const location = useLocation();
+
+  const { q: heroToQuery = '' } = queryString.parse(location.search);
+
+  const { searchText, onInputChange } = useForm({
+    searchText: ''
+  });
+
+  const onSearchSubmit = (event) => {
+    event.preventDefault();
+
+    if (searchText.trim().length <= 1) return;
+
+    navigate(`?q=${searchText.toLowerCase().trim()}`);
+  }
+
   return (
-    <h1>Search</h1>
+    <>
+      <h1> Search </h1>
+      <hr />
+
+      <div className="row">
+        <div className="col-5">
+          <h4> Searching </h4>
+          <hr />
+          <form onSubmit={ onSearchSubmit }>
+            <input
+              type="text"
+              placeholder="Search a Hero"
+              className="form-control"
+              name="searchText"
+              autoComplete="off"
+              value={searchText}
+              onChange={ onInputChange }
+            />
+
+            <button className="btn btn-outline-primary mt-1"> Search </button>
+          </form>
+        </div>
+
+        <div className="col-7">
+          <h4> Results </h4>
+          <hr />
+          <div className="alert alert-primary"> Search a Hero </div>
+          <div className="alert alert-danger"> There're not results with { heroToQuery } </div>
+
+          {/* <HeroCard /> */}
+        </div>
+      </div>
+    </>
   )
 }
