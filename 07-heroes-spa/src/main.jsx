c70a759ed08ace3bf6734e1986a6fdb4b8e3fc64
Moises Alvarado <moisesalvarado84@gmail.com>
2024-02-12 17:36:23 -0600

    Adding a context to handle the auth

--- 07-heroes-spa/src/main.jsx
+++ 07-heroes-spa/src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles.css';
 import { AppRouter } from './router/AppRouter';
+import { AuthProvider } from './auth';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <AuthProvider>
+    
     <AppRouter />
-  </React.StrictMode>,
+
+  </AuthProvider>,
 )
