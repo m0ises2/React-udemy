c8b8c9c4a06e359940b5c60bd05242f73567d853
Moises Alvarado <moisesalvarado84@gmail.com>
2024-02-14 17:38:20 -0600

    adding unit test to cover the types list

---
+++ 07-heroes-spa/tests/auth/context/types/types.test.js
@@ -0,0 +1,10 @@
+import { types } from "../../../../src/auth/types/types";
+
+describe('types tests', () => {
+  test('types should be the listed ones', () => {
+    expect(types).toEqual({
+      login: '[Auth] Login',
+      logout: '[Auth] Logout'
+    });
+  });
+});
