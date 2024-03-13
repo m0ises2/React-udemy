a5787d4be96618e22a1eef1867c4d5cd953faf63
Moises Alvarado <moisesalvarado84@gmail.com>
2024-03-13 17:07:15 -0600

    Adding the NavBar component to the journalLayout

--- 08-journal-app/src/journal/layout/JournalLayout.jsx
+++ 08-journal-app/src/journal/layout/JournalLayout.jsx
@@ -1,9 +1,12 @@
-import { Box } from "@mui/material"
+import { Box } from '@mui/material'
+import { NavBar } from '../components';
+
+const drawerWidth = 240;
 
 export const JournalLayout = ({ children }) => {
   return (
     <Box sx={{ display: 'flex' }}>
-      { /* Navbar */}
+      <NavBar drawerWidth={ drawerWidth } />
 
       { /* sidebar */}
 
