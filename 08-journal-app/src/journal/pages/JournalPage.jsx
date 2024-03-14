88c057eff0dc7062c8ab12f1ad5243e260b7eb11
Moises Alvarado <moisesalvarado84@gmail.com>
2024-03-14 17:21:44 -0600

    Adding a floating button to the journalPage component

--- 08-journal-app/src/journal/pages/JournalPage.jsx
+++ 08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,4 +1,5 @@
-import { Typography } from '@mui/material';
+import { Typography, IconButton } from '@mui/material';
+import { AddOutlined } from '@mui/icons-material';
 import { JournalLayout } from '../layout/JournalLayout';
 import { NoteView, NothingSelectedView } from '../views';
 
@@ -7,8 +8,24 @@ export const JournalPage = () => {
     <JournalLayout>
       {/* <Typography > JournalPageJournalPageJournalPageJournalPageJournalPageJournalPage </Typography> */}
 
-      {/* <NothingSelectedView /> */}
-      <NoteView />
+      <NothingSelectedView />
+     
+      {/* <NoteView /> */}
+
+      <IconButton
+        size='large'
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }}/>
+      </IconButton>
+
     </JournalLayout>
   )
 }
