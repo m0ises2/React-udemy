e00ae63f3043920f7c8e07d3b594aca419e48e73
Moises Alvarado <moisesalvarado84@gmail.com>
2024-04-08 17:44:41 -0600

    initializing project with redux pattern

---
+++ 09-toolkit-redux/src/App.jsx
@@ -0,0 +1,32 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { increment } from './store/slices/counter/counterSlice'
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const { counter } = useSelector((state) => state.counter)
+  const dispatch = useDispatch()
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => dispatch(increment())}>
+          count is { counter }
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
