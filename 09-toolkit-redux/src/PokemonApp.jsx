651d785a36e1ef4f768e2bc0be7b7796bfeeaf10
Moises Alvarado <moisesalvarado84@gmail.com>
2024-04-11 17:17:53 -0600

    creating the pokemonApp component. Creating the pokemonSlice slice and adding two reducers to it

---
+++ 09-toolkit-redux/src/PokemonApp.jsx
@@ -0,0 +1,16 @@
+
+export const PokemonApp = () => {
+  return (
+    <>
+        <h1> PokemonApp </h1>
+
+        <hr />
+
+        <ul>
+            <li> Hola </li>
+            <li> Hola </li>
+            <li> Hola </li>
+        </ul>
+    </>
+  )
+}
--- 09-toolkit-redux/src/main.jsx
+++ 09-toolkit-redux/src/main.jsx
@@ -1,14 +1,16 @@
-import { Provider } from 'react-redux'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { Provider } from 'react-redux';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { PokemonApp } from './PokemonApp.jsx';
 import { store } from './store/store';
-import './index.css'
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      {/* <App /> */}
+      <PokemonApp />
     </Provider>
   </React.StrictMode>,
 )
